d46da00943f5aeb1ce7f5802083184fcbdb8f3e9
kim-yeon-gyu-exlock <public.kim.yeon.gyu@gmail.com>
2018-05-29 22:52:12 +0900

    added: get schoolcode from user

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from neispy import *
 day = int(input("덕현중학교의 급식이 궁금하세요? 무슨요일 급식이 궁금하세요?\n월 : 0, 화 : 1, 수 : 2, 목 : 3, 금 : 4\n여기에 입력하세요 : "))
+schoolcode = int(input("학교 코드를 입력해주세요 : "))
 if day < 0 or day > 4 :
     print("수를 제대로 입력해주세요.")
     exit()
 day = day + 7
-o = get_meals(day, "J100005133")
+o = get_meals(day, schoolcode)
 print(o)
